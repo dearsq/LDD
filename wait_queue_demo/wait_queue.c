6d15a895a9fbc200b2c334872a592b9fb8877a53
Younix <foreveryounix@gmail.com>
2016-10-18 15:27:26 +0800

    wait_queue demo 用等待队列实现读写设备缓冲区

---
+++ wait_queue_demo/wait_queue.c
@@ -0,0 +1,9 @@
+/*************************************************************************
+    > File Name: wait_queue.c
+    > Author: Younix
+    > Mail: foreveryounix@gmail.com 
+    > Created Time: 2016年10月18日 星期二 14时13分58秒
+    > 作用： 等待队列的实现
+ ************************************************************************/
+
+static ssize_t xxx_write(struct file *file, const char *buffer, size_t count, loff_t *ppos)
